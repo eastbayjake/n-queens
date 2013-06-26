71da7b37dacab8a621f482dcd1f1f376bb54f9a9
EpiphanyMachine <EpiphanyMachine@gmail.com>
2013-06-26 12:38:11 -0700

    Recursively find one solution, WIP: all solutions

--- awesome.js
+++ awesome.js
@@ -1,5 +1,8 @@
 var rooks = function(n){
-  this.blankBoard = function(n){
+  var needToTest = this.blankBoard();
+  var results = [];
+
+  this.blankBoard = function(){
     var blankBoard = [];
     for (var i = 0; i < n; i++) {
       blankBoard[i] = [];
@@ -8,13 +11,13 @@ var rooks = function(n){
       }
     }
     return blankBoard;
-  }
+  };
 
-  this.nextTestSpace = function(){
+  this.nextTestSpace = function(board){
     for (var i = 0; i < n; i++) {
-      if (!blankBoard[i]) {
+      if (!board[i]) {
         for (var j = 0; j < n; j++) {
-          if (blankBoard[i][j]) {
+          if (board[i][j]) {
             return [i, j];
           }
         }
@@ -22,7 +25,33 @@ var rooks = function(n){
     }
   };
 
-  // these are storage variables
-  var needToTest = this.blankBoard(n);
+  this.testSpace = function(){
 
+    var rooksPlaced = [];
+    var testBoard = this.blankBoard();
+
+    var testNextSpace = function(tempSpace){
+      tempSpace = tempSpace || this.nextTestSpace(needToTest);
+      rooksPlaced.push(tempSpace);
+      testBoard[tempSpace[0]] = undefined;
+      for (var i = 0; i < testBoard.length; i++) {
+        if (testBoard[tempSpace[0]] !== undefined) {
+          testBoard[i][tempSpace[1]] = undefined;
+        }
+      }
+      var nextTempSpace = this.nextTestSpace(testBoard);
+      if (nextTempSpace) {
+        testNextSpace(nextTempSpace);
+      }
+    };
+
+    testNextSpace();
+
+    if (rooksPlaced.length === n) {
+      results.push(rooksPlaced);
+    }
+
+  };
+
+  // these are storage variables
 };
